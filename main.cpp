efe9266081acf9d6b60c1d7206a424a650e2e4e2
Daniel Miranda <danielvcmiranda@gmail.com>
2019-11-23 20:16:24 -0300

    lendo arquivo para testar

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 #include <fstream>
 
 int main() {
+    std::ifstream f("../Database/Usuarios/usuarios.csv",std::ios::in);
+    char c[200];
+    if(f.fail()){
+        std::cout <<"no file"<<std::endl;
+    }
+    else {
+        while (!f.eof()) {
+            f.getline(c, 200);
+            std::cout << c << std::endl;
+        }
+    }
+    f.close();
     return 0;
 }
