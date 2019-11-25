c58156deb1170d16786122b27608bfce709340b9
Daniel Miranda <danielvcmiranda@gmail.com>
2019-11-24 21:02:49 -0300

    coisas do CLion

---
+++ cmake-build-debug/CMakeFiles/20192_team_10.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "20192_team_10.exe"
+  "20192_team_10.exe.manifest"
+  "20192_team_10.pdb"
+  "CMakeFiles/20192_team_10.dir/Usuario.cpp.obj"
+  "CMakeFiles/20192_team_10.dir/main.cpp.obj"
+  "lib20192_team_10.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20192_team_10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
