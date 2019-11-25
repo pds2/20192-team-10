217b15ca9bcaeecc1bd6af55c402df58b340264f
Daniel Miranda <danielvcmiranda@gmail.com>
2019-11-25 16:45:38 -0300

    melhorando codigo de fazer login de organizador

--- Organizador.cpp
+++ Organizador.cpp
@@ -4,6 +4,7 @@
 
 #include "Organizador.h"
 #include "Usuario.h"
+#include "Csv.h"
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -19,68 +20,20 @@ Organizador::Organizador() {
     this->name = "NINGUEM";
 }
 void Organizador::fazerLogin(std::string username) {
-    std::ifstream f("../Database/Usuarios/organizadores.csv",std::ios::in);
-    char linha[200], categoria[50];
-    std::string category;
-    int i =0, j=0, k=0, u_id =0;
-    if(f.fail()){
-        std::cout <<"no file"<<std::endl;
+    int i;
+    std::string file_path = "../Database/Usuarios/organizadores.csv";
+    std::vector<std::vector<std::string>> registros = parse_csv(file_path);
+    if (registros.empty()){
+        return;
     }
-    else {
-        f.getline(linha, 200);//tira o cabeçalho
-        while (!f.eof()) {
-            f.getline(linha, 200);
-            for(j=0;j<200;j++){
-                if(linha[j] != ','){
-                    if((linha[j] == ' ' && i!=2) || linha[j] == '"'){
-                        continue;
-                    }
-                    categoria[k]=linha[j];
-                    k++;
-                    continue;
-                }
-                else if (i==0){
-                    u_id = atoi(categoria);
-                    for (k=0;k<j;k++){ //zera string
-                        categoria[k] ='\0';
-                    }
-                    i++;
-                    k=0;
-                }
-                else if(i==1){
-                    categoria[k]='\0';
-                    category = categoria;
-                    if (category == username){
-                        this->username = username;
-                        for(i=0;i<k;i++){
-                            categoria[i]='\0';
-                        }
-                        i=2;
-                        k=0;
-                        f.close();
-
-                    }
-                    else {
-                        for (i = 0; i < k; i++) {
-                            categoria[i] = '\0';
-                        }
-                        i = 0;
-                        k = 0;
-                        break;
-                    }
-                }
-                else{
-                    categoria[k]='\0';
-                    category = categoria;
-                    this->name = category;
-                    this->id = u_id;
-                    std::cout << "Login feito com sucesso!"<< std::endl;
-                    f.close();
-                    return;
-                }
-            }
+    for (i =0;i<registros.size();i++){
+        if (registros.at(i).at(1)==username){
+            this->id = stoi(registros.at(i).at(0));
+            this->username = registros.at(i).at(1);
+            this->name = registros.at(i).at(2);
+            std::cout << "Olá "<< this->name << std::endl;
+            return;
         }
     }
-    f.close();
     std::cout << "não foi encontrado nenhum usuario com esse username"<< std::endl;
 }
