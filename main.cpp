dfcc82740f1da0ff0fc87fdd4ad8f3621a31ce2a
Daniel Miranda <danielvcmiranda@gmail.com>
2019-11-25 22:45:29 -0300

    testando tipo de usuario premium

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 #include "Usuario.h"
 #include "Organizador.h"
 #include "Free.h"
+#include "Premium.h"
 #include "Csv.h"
 
 int main() {
     std::string user_name;
     std::vector<std::vector<std::string>> resposta;
     std::cin >> user_name;
-    Free user = Free();
+    Premium user = Premium();
     user.fazerLogin(user_name);
     std::cout << user.get_name() << std::endl;
     std::cout << user.get_username() << std::endl;
