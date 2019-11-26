106a1a32732ea255d1186470c972ab5d889b43ac
Daniel Miranda <danielvcmiranda@gmail.com>
2019-11-25 22:14:13 -0300

    testando classe de usuario Free

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 #include <vector>
 #include "Usuario.h"
 #include "Organizador.h"
+#include "Free.h"
 #include "Csv.h"
 
 int main() {
     std::string user_name;
     std::vector<std::vector<std::string>> resposta;
     std::cin >> user_name;
-    Organizador user = Organizador();
+    Free user = Free();
     user.fazerLogin(user_name);
-    std::cout << user.get_id() << std::endl;
     std::cout << user.get_name() << std::endl;
     std::cout << user.get_username() << std::endl;
     return 0;
