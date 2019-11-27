ec89567aeac20602fe3ba077aede7e640950a500
Daniel Miranda <danielvcmiranda@gmail.com>
2019-11-26 22:57:28 -0300

    testando cadastro de usuario no main

--- main.cpp
+++ main.cpp
@@ -10,10 +10,19 @@
 int main() {
     std::string user_name;
     std::vector<std::vector<std::string>> resposta;
-    std::cin >> user_name;
-    Premium user = Premium();
-    user.fazerLogin(user_name);
-    std::cout << user.get_name() << std::endl;
-    std::cout << user.get_username() << std::endl;
+    std::getline(std::cin,user_name);;
+    std::string nome;
+    std::string i = "1";
+    Organizador user = Organizador();
+    user.fazerLogin("daniel");
+//    std::cout<<"Nome do novo usuario: "<<std::endl;
+//    std::getline(std::cin,nome);
+//    std::cout<<"tipo de usuario:"<<std::endl;
+//    std::getline(std::cin,i);
+//    std::cout<<"username: "<<std::endl;
+//    std::getline(std::cin,user_name);
+    user.cadastrarNovoUsuario("jeanine","JEANINE VASCONCELOS",stoi(i));
+    user.cadastrarNovoUsuario("paulo", "PAULO MIRANDA", 2);
+    user.cadastrarNovoUsuario("rachel", "RACHEL MARINHO", 3);
     return 0;
 }
