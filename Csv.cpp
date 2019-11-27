2cb1f8e217c1a2c3a169caeea833a8d5e4f9769c
Daniel Miranda <danielvcmiranda@gmail.com>
2019-11-26 22:55:46 -0300

    adicionando novas funçoes a csv

--- Csv.cpp
+++ Csv.cpp
@@ -1,4 +1,5 @@
 #include "Csv.h"
+#include "Usuario.h"
 #include <string>
 #include <vector>
 #include <iostream>
@@ -33,3 +34,50 @@ std::vector<std::vector<std::string>> parse_csv(std::string path_arquivo){
     f.close();
     return resposta;
   }
+
+std::vector<std::string> encontrarUsuario(std::string username, std::string filepath) {
+    unsigned int i;
+    std::vector<std::vector<std::string>> registros = parse_csv(filepath);
+    std::vector<std::string> nenhum = {" "};
+    if (registros.empty()){
+        return nenhum;
+    }
+    for (i =0;i<registros.size()-1;i++){
+        if (registros.at(i).at(0)==username) {
+            return registros.at(i);
+        }
+    }
+    return  nenhum;
+}
+
+void insereUsuario(std::string username, std::string nome, std::string filepath){
+    std::string linha = username+","+nome+",0\n";
+    std::ofstream f(filepath,std::ios::app);
+    if(f.fail()){
+        std::cout <<"no file"<<std::endl;
+        return;
+    }
+    f<<linha;
+}
+
+void insereUsuario(std::string username,  std::string filepath, int tipo){
+    std::ofstream f(filepath,std::ios::app);
+    if(f.fail()){
+        std::cout <<"no file"<<std::endl;
+        return;
+    }
+    switch(tipo){
+        case 1:
+            f<<username<<",organizador\n";
+            break;
+        case 2:
+            f<<username<<",premium\n";
+            break;
+        case 3:
+            f<<username<<",free\n";
+            break;
+        default:
+            return;
+    }
+
+}
--- Csv.h
+++ Csv.h
@@ -1,5 +1,8 @@
 #include <string>
 #include <vector>
+#include "Usuario.h"
 
 std::vector<std::vector<std::string>> parse_csv(std::string path_arquivo);
-
+std::vector<std::string> encontrarUsuario (std::string username, std::string filepath);
+void insereUsuario(std::string username, std::string nome, std::string filepath);
+void insereUsuario(std::string username, std::string filepath, int tipo);
